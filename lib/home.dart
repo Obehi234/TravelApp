d89f38e97c98e2fcd382c5ab7208b7f3d2b6fa76
obehi234 <obehi.ataga234@gmail.com>
2024-01-13 18:49:36 +0100

    Updated UI

--- lib/home.dart
+++ lib/home.dart
@@ -16,35 +16,43 @@ class _HomePageState extends State<HomePage> {
         child: Container(
           color: Colors.orangeAccent,
           child: Padding(
-            padding: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 42),
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Icon(Icons.loop, color: Colors.white, size: 30),
-              ),
-              const Text(
-                'GIVSUM',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  decoration: TextDecoration.none,
+              Center(child: Icon(Icons.loop, color: Colors.white, size: 32)),
+              const Center(
+                child:  Text(
+                  'GIVSUM',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      decoration: TextDecoration.none,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              Image.asset('images/scene1.png', fit: BoxFit.contain),
-              const Text(
-                'Sign Up',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 22,
-                    decoration: TextDecoration.none),
+              SizedBox(height: 16),
+              Image.asset(
+                'images/scene1.png',
+                fit: BoxFit.contain,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: const Text(
+                  'Sign Up',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      decoration: TextDecoration.none,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
               TextField(
                 decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email),
                     filled: true,
                     fillColor: Colors.white.withOpacity(0.5),
                     contentPadding:
-                        EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(50.0),
                         borderSide: BorderSide.none),
@@ -54,12 +62,16 @@ class _HomePageState extends State<HomePage> {
                         fontSize: 14,
                         fontWeight: FontWeight.w200)),
               ),
+              SizedBox(height: 16),
               TextField(
+                keyboardType: TextInputType.name,
+                textInputAction: TextInputAction.done,
                 decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person),
                     filled: true,
                     fillColor: Colors.white.withOpacity(0.5),
                     contentPadding:
-                        EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(50.0),
                         borderSide: BorderSide.none),
@@ -69,30 +81,37 @@ class _HomePageState extends State<HomePage> {
                         fontSize: 14,
                         fontWeight: FontWeight.w200)),
               ),
+              SizedBox(height: 26),
               TextField(
+                keyboardType: TextInputType.phone,
+                textInputAction: TextInputAction.done,
                 decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.phone),
                     filled: true,
                     fillColor: Colors.white.withOpacity(0.5),
                     contentPadding:
-                        EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(50.0),
                         borderSide: BorderSide.none),
-                    hintText: 'Full Name',
+                    hintText: 'Mobile Number',
                     hintStyle: TextStyle(
                         color: Colors.grey,
                         fontSize: 14,
                         fontWeight: FontWeight.w200)),
               ),
-              Row(children: [
-                Checkbox(
-                  value: false,
-                  onChanged: null,
-                ),
-                Text(
-                    'I agree to the Terms of Service and Privacy Policy, only\n after reading the terms and conditions can you continue',
-                    style: TextStyle(color: Colors.white, fontSize: 10))
-              ]),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: [
+                  Checkbox(
+                    value: false,
+                    onChanged: null,
+                  ),
+                  Text(
+                      'I agree to the Terms of Service and Privacy Policy, \nonly after reading can you continue',
+                      style: TextStyle(color: Colors.white, fontSize: 10))
+                ]),
+              ),
               Center(
                 child: OutlinedButton(
                     style: OutlinedButton.styleFrom(
@@ -111,6 +130,21 @@ class _HomePageState extends State<HomePage> {
                       ),
                     )),
               ),
+              SizedBox(height: 18),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Already have an account?',
+                        style: TextStyle(color: Colors.white, fontSize: 14)),
+                    SizedBox(width: 4),
+                    Text('Login',
+                        style: TextStyle(
+                            color: Colors.blueGrey.shade600,
+                            fontWeight: FontWeight.bold))
+                  ],
+                ),
+              )
             ]),
           ),
         ),
